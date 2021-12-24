3c64504dad879ac62d422d7022cf3ea776510106
Anastasia Poimanova <94190936+nastia4464@users.noreply.github.com>
2021-12-24 11:01:10 +0200

    Update 11-review.js

--- lecture_notes/11 review/11-review.js
+++ lecture_notes/11 review/11-review.js
@@ -1,18 +1,16 @@
 const plane = (arr) => {
-    const res = []
-    for (let i = 0, length = arr.length; i < length; i++) {
-        let value = arr[i];
-        if (Array.isArray(value)) {
-            res.push(...plane(value));
-            arr[i] = res[i - 1];
-        }
-        else {
-            arr[i] = res[i - 1]
-            res.push(value);
-        }
+  const res = [];
+  for (let i = 0, length = arr.length; i < length; i++) {
+    let value = arr[i];
+    if (Array.isArray(value)) {
+      res.push(...plane(value));
+    } else {
+      arr[i] = res[i - 1];
+      res.push(value);
     }
-    return res;
-}
+  }
+  return res;
+};
 
 const result = plane([1, [2], [3, 4], [5], [6, [7, 8]]]);
 console.log(result);
